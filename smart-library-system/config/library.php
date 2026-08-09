1dc28d8d743471f8a68138b32a6a878e1b05e5db
JohnLaizy <johnzhenyiklai@gmail.com>
2026-08-10 00:10:28 +0800

    add service classes

--- smart-library-system/config/library.php
+++ smart-library-system/config/library.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-    //
+    'borrow_limit' => (int) env(
+        'LIBRARY_BORROW_LIMIT',
+        5
+    ),
+
+    'loan_days' => (int) env(
+        'LIBRARY_LOAN_DAYS',
+        7
+    ),
+
+    'overdue_fee_cents_per_day' => (int) env(
+        'LIBRARY_OVERDUE_FEE_CENTS_PER_DAY',
+        100
+    ),
 ];
