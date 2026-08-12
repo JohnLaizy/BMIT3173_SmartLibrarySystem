0d2a53d01b715049cd56d13297087ea6ef0450a2
jylee2705 <samlee.jy@gmail.com>
2026-08-12 16:40:14 +0800

    Improve room booking navigation UI

--- smart-library-system/resources/views/bookings/index.blade.php
+++ smart-library-system/resources/views/bookings/index.blade.php
@@ -2,154 +2,211 @@
 
     <div class="mx-auto flex w-full max-w-6xl flex-1 flex-col gap-8 px-2 sm:px-4">
 
-        <div>
-            <flux:heading size="xl">
-                Room Booking
-            </flux:heading>
 
-            <flux:text class="mt-2">
-                Manage library room bookings.
-            </flux:text>
+        {{-- Header --}}
+        <div class="flex items-center justify-between">
+
+
+            <div>
+
+                <flux:heading size="xl">
+                    Room Booking
+                </flux:heading>
+
+
+                <flux:text class="mt-2">
+                    Manage library room bookings.
+                </flux:text>
+
+
+            </div>
+
+
+
+            {{-- Create Booking Button --}}
+            <flux:button href="{{ route('bookings.create') }}" variant="primary">
+                + Book Room
+            </flux:button>
+
+
         </div>
 
 
-        @if(session('success'))
+
+
+
+        {{-- Success Message --}}
+        @if (session('success'))
             <div class="rounded-lg bg-green-50 px-4 py-3 text-green-800">
+
                 {{ session('success') }}
+
             </div>
         @endif
 
 
-        @if(session('error'))
+
+
+
+
+        {{-- Error Message --}}
+        @if (session('error'))
             <div class="rounded-lg bg-red-50 px-4 py-3 text-red-800">
+
                 {{ session('error') }}
+
             </div>
         @endif
 
 
 
+
+
+
+
+
+        {{-- Booking Table --}}
         <div class="rounded-xl border border-zinc-200 bg-white p-5">
+
 
             <flux:table>
 
+
                 <flux:table.columns>
+
 
                     <flux:table.column>
                         User
                     </flux:table.column>
 
+
                     <flux:table.column>
                         Room
                     </flux:table.column>
+
 
                     <flux:table.column>
                         Date
                     </flux:table.column>
 
+
                     <flux:table.column>
                         Time
                     </flux:table.column>
+
 
                     <flux:table.column>
                         Status
                     </flux:table.column>
 
+
                     <flux:table.column>
                         Action
                     </flux:table.column>
 
+
                 </flux:table.columns>
+
+
 
 
 
                 <flux:table.rows>
 
 
-                    @foreach($bookings as $booking)
-
-                    <flux:table.row>
-
-
-                        <flux:table.cell>
-                            {{ $booking->user->name }}
-                        </flux:table.cell>
+                    @foreach ($bookings as $booking)
+                        <flux:table.row>
 
 
-
-                        <flux:table.cell>
-                            {{ $booking->room->room_number }}
-                        </flux:table.cell>
+                            <flux:table.cell>
+                                {{ $booking->user->name }}
+                            </flux:table.cell>
 
 
 
-                        <flux:table.cell>
-                            {{ $booking->booking_date }}
-                        </flux:table.cell>
+
+                            <flux:table.cell>
+                                {{ $booking->room->room_number }}
+                            </flux:table.cell>
 
 
 
-                        <flux:table.cell>
-                            {{ $booking->start_time }}
-                            -
-                            {{ $booking->end_time }}
-                        </flux:table.cell>
+
+                            <flux:table.cell>
+                                {{ $booking->booking_date }}
+                            </flux:table.cell>
 
 
 
-                        <flux:table.cell>
-                            {{ ucfirst($booking->status) }}
-                        </flux:table.cell>
 
+                            <flux:table.cell>
 
-
-                        <flux:table.cell>
-
-
-                            @if($booking->status == 'confirmed')
-
-                                <form method="POST"
-                                      action="/bookings/{{ $booking->id }}/cancel">
-
-                                    @csrf
-                                    @method('PATCH')
-
-
-                                    <flux:button 
-                                        type="submit"
-                                        variant="danger">
-
-                                        Cancel
-
-                                    </flux:button>
-
-
-                                </form>
-
-
-                            @else
-
+                                {{ $booking->start_time }}
                                 -
+                                {{ $booking->end_time }}
 
-                            @endif
-
-
-                        </flux:table.cell>
+                            </flux:table.cell>
 
 
 
-                    </flux:table.row>
 
 
+                            <flux:table.cell>
+
+                                {{ ucfirst($booking->status) }}
+
+                            </flux:table.cell>
+
+
+
+
+
+
+
+                            <flux:table.cell>
+
+
+                                @if ($booking->status == 'confirmed')
+                                    <form method="POST" action="/bookings/{{ $booking->id }}/cancel">
+
+
+                                        @csrf
+                                        @method('PATCH')
+
+
+                                        <flux:button type="submit" variant="danger">
+
+                                            Cancel
+
+                                        </flux:button>
+
+
+                                    </form>
+                                @else
+                                    -
+                                @endif
+
+
+                            </flux:table.cell>
+
+
+
+
+
+                        </flux:table.row>
                     @endforeach
 
 
+
                 </flux:table.rows>
+
 
 
             </flux:table>
 
 
         </div>
+
 
 
     </div>
